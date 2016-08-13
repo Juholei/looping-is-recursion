bc46cc5307722a40a21d872221af5665656482ce
Juho Leinonen <juholei@gmail.com>
2016-08-13 13:30:10 +0300

    4 exercises done

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -8,13 +8,27 @@
     (helper 1 base exp)))
 
 (defn last-element [a-seq]
-  ":(")
+  (let [helper (fn [acc a-seq]
+                 (if (empty? a-seq)
+                   acc
+                   (recur (first a-seq) (rest a-seq))))]
+    (helper (first a-seq) (rest a-seq))))
 
 (defn seq= [seq1 seq2]
-  ":(")
+  (cond
+    (and (empty? seq1) (empty? seq2)) true
+    (not= (empty? seq1) (empty? seq2)) false
+    (not= (first seq1) (first seq2)) false
+    :else (recur (rest seq1) (rest seq2))))
 
 (defn find-first-index [pred a-seq]
-  ":(")
+  (loop [acc 0
+         pred pred
+         a-seq a-seq]
+    (cond
+      (empty? a-seq) nil
+      (pred (first a-seq)) acc
+      :else (recur (inc acc) pred (rest a-seq)))))
 
 (defn avg [a-seq]
   -1)
