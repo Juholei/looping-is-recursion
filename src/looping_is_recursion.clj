db0499d0a54ecf1fe3ef26985033964f913406b6
Juho Leinonen <juholei@gmail.com>
2016-08-14 15:14:39 +0300

    All exercises done

--- src/looping_is_recursion.clj
+++ src/looping_is_recursion.clj
@@ -31,14 +31,39 @@
       :else (recur (inc acc) pred (rest a-seq)))))
 
 (defn avg [a-seq]
-  -1)
+  (loop [acc 0
+         size 0
+         a-seq a-seq]
+    (if (empty? a-seq)
+      (/ acc size)
+      (recur (+ acc (first a-seq)) (inc size) (rest a-seq)))))
+
+(defn toggle [a-set elem]
+  (if (contains? a-set elem)
+    (disj a-set elem)
+    (conj a-set elem)))
 
 (defn parity [a-seq]
-  ":(")
+  (loop [acc #{}
+         a-seq a-seq]
+    (cond
+      (empty? a-seq) acc
+      :else (recur (toggle acc (first a-seq)) (rest a-seq)))))
 
 (defn fast-fibo [n]
-  ":(")
+  (loop [prev 0
+         cur 1
+         n n]
+    (cond
+      (zero? n) prev
+      :else (recur cur (+ prev cur) (dec n)))))
 
 (defn cut-at-repetition [a-seq]
-  [":("])
+  (loop [seen #{}
+         return []
+         a-seq a-seq]
+    (cond
+      (empty? a-seq) return
+      (contains? seen (first a-seq)) return
+      :else (recur (conj seen (first a-seq)) (conj return (first a-seq)) (rest a-seq)))))
 
